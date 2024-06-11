730b589a7fda9aba2c941a9ecbda3f04815ebda2
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-11 16:36:15 +0530

    Style the title

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,14 +5,23 @@ import 'package:flutter/widgets.dart';
 
 
 class HomePage extends StatelessWidget {
+
+  late double _pageHeight, _pagewidth;
   
-    const HomePage({Key? key}) : super(key:key);
+     HomePage({Key? key}) : super(key:key);
 
     @override
   Widget build(BuildContext context) {
+
+    _pageHeight = MediaQuery.of(context).size.height;
+    _pagewidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: SafeArea(
         child: Container(
+          height: _pageHeight,
+          width: _pagewidth,
+          padding: EdgeInsets.symmetric(horizontal: _pagewidth*0.08),
           child: _pageTitle() ,
           )
         )
@@ -26,7 +35,8 @@ class HomePage extends StatelessWidget {
       fontSize: 70,
       fontWeight: FontWeight.w700,
 
-    ),);
+    ),
+    );
   }
 
   Widget _astroImageWidget(){
