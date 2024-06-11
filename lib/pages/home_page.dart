f3a5ee8d2b537f05babecee0fbe8fb67f95f779c
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-11 15:00:22 +0530

    Creating the title

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 
 
@@ -10,14 +11,23 @@ class HomePage extends StatelessWidget {
     @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: _astroImageWidget(),
+    );
+  }
+
+
+  Widget _pageTitle(){
+    return Text("#Go Moon");
+  }
+
+  Widget _astroImageWidget(){
+    return  Container(
         decoration: const BoxDecoration(
           image: DecorationImage( 
             image: AssetImage("assets/images/background.jpg"),
-            fit: BoxFit.cover,
+            fit: BoxFit.fill,
             ),
         ),
       ),
-    );
   }
 }
