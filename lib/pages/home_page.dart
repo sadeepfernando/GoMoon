c7a2c0ccc6f14a24970b33862deacdfa7346f01e
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-13 18:07:04 +0530

    Dropdown container decoration

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,71 +1,78 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-
-
 class HomePage extends StatelessWidget {
-
   late double _pageHeight, _pagewidth;
-  
-     HomePage({Key? key}) : super(key:key);
 
-    @override
+  HomePage({Key? key}) : super(key: key);
+
+  @override
   Widget build(BuildContext context) {
-
     _pageHeight = MediaQuery.of(context).size.height;
     _pagewidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          height: _pageHeight,
-          width: _pagewidth,
-          padding: EdgeInsets.symmetric(horizontal: _pagewidth*0.08),
-          child:  Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-                _pageTitle(),
-                _destinationDropDownWidget()
-            ],) ,
-          )
-        )
+        body: SafeArea(
+            child: Container(
+      height: _pageHeight,
+      width: _pagewidth,
+      padding: EdgeInsets.symmetric(horizontal: _pagewidth * 0.08),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [_pageTitle(), _destinationDropDownWidget()],
+      ),
+    )));
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#Go Moon",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 70,
+        fontWeight: FontWeight.w700,
+      ),
     );
   }
 
-
-  Widget _pageTitle(){
-    return const Text("#Go Moon",style: TextStyle(
-      color: Colors.white,
-      fontSize: 70,
-      fontWeight: FontWeight.w700,
-
-    ),
-    );
-  }
-
-  Widget _astroImageWidget(){
-    return  Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage( 
-            image: AssetImage("assets/images/background.jpg"),
-            fit: BoxFit.fill,
-            ),
-        ),
-      );
-  }
-
-  Widget _destinationDropDownWidget(){
-
-    List<DropdownMenuItem<String>> _Items = ['SSF Web Station', 'BUAC Web Station']
-                                            .map((e){return DropdownMenuItem(
-                                              value: e,
-                                              child: Text(e),);})
-                                              .toList();
+  Widget _astroImageWidget() {
     return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/background.jpg"),
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+
+  Widget _destinationDropDownWidget() {
+    // ignore: non_constant_identifier_names
+    List<String> Items = ['SSF Web Station', 'BUAC Web Station'];
+
+    return Container(
+      //Container decoration
+      decoration: BoxDecoration(
+          color:const Color.fromRGBO(53, 53, 53, 1.0),
+          borderRadius: BorderRadius.circular(10)),
+
       child: DropdownButton(
-        onChanged: (_){},
-        items: _Items ,)
-        ,);
+        //Styling the dropdown button
+        underline: Container(),
+        value: Items.first,
+        dropdownColor: const Color.fromRGBO(53, 53, 53, 1.0),
+        style: const TextStyle(color: Colors.white),
+
+        onChanged: (_) {},
+        items: Items.map((e) {
+          return DropdownMenuItem(
+            value: e,
+            child: Text(e),
+          );
+        }).toList(),
+      ),
+    );
   }
 }
